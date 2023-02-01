26cbf14c9d28f89c161c693d22c69c618dc89d97
Praveen-711 <110688702+Praveen-711@users.noreply.github.com>
2023-02-01 20:51:12 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "myvpc" {
-  cidr_block = "172.16.0.0/12"
+  cidr_block = "192.168.0.0/16"
 }
 
 resource "aws_subnet" "vpcsubnet" {
   vpc_id     = aws_vpc.myvpc.id
-  cidr_block = "172.16.0.0/24"
+  cidr_block = "192.168.0.0/24"
   }
